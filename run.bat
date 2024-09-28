5a236f19b96cb7ed4a5667094fed1efe205b3507
TheNailpepino <167659529+KingPepinoo@users.noreply.github.com>
2024-09-27 20:46:48 -0700

    run.bat for dummies
    just open the run.bat and then it will launch the python script and all you need to do is put in the .jar file and then press enter boom

---
+++ run.bat
@@ -0,0 +1,14 @@
+@echo off
+REM Get the directory of the batch file
+set "script_dir=%~dp0"
+
+REM Change to that directory
+cd /d "%script_dir%"
+
+REM Run the Python script
+python run.py
+
+REM Keep the command prompt open
+echo.
+echo Press any key to continue...
+pause >nul
